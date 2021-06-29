ea4ea8d1d406cc61617b9eefab7c302bfbe162a4
Rajeev Tikka <rajeevtikka@yahoo.com>
2021-06-29 11:27:36 -0400

    added icons to sidebar

--- src/components/list-sidebar/listsidebar.component.jsx
+++ src/components/list-sidebar/listsidebar.component.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import {AiOutlinePlusSquare, AiOutlineCloudUpload} from 'react-icons/ai'
+import {GiCheckboxTree} from 'react-icons/gi'
 import './listsidebar.styles.scss'
 
 const ListSideBar = () => (
     <div className = "list-sidebar">
         <ul>
-            <li className = "list-sidebar-element">Ruleset</li>
-            <li className = "list-sidebar-element">Create Rules</li>
-            <li className = "list-sidebar-element">Upload Rules</li>
+            <li className = "list-sidebar-element">
+                <span><GiCheckboxTree /></span>
+                <span className = "list-sidebar-text">Ruleset</span>
+            </li>
+            <li className = "list-sidebar-element">
+                <span><AiOutlinePlusSquare /></span>
+                <span className = "list-sidebar-text">Create Rules</span>
+            </li>
+            <li className = "list-sidebar-element">
+                <span><AiOutlineCloudUpload /></span>
+                <span className = "list-sidebar-text">Upload Rules</span>
+            </li>
         </ul>
     </div>
 )
